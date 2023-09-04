781b1768e601df6aed62d99c745ace94fe4edce1
Rajasree2004 <rajasree.selva@gmail.com>
2023-09-04 22:12:18 +0530

    day 20 prob 1

--- Day20/move-zeros.py
+++ Day20/move-zeros.py
@@ -43,7 +43,7 @@ class Solution:
         i = 0
         for n in nums:
             if n != 0:
-                nums[index] = n
+                nums[i] = n
                 i += 1
         while i < len(nums):
             nums[i] = 0
