a18a646c950d247aca540e3d3f1e14543e74f4be
Rajasree2004 <rajasree.selva@gmail.com>
2023-09-23 13:15:00 +0530

    day 24 prob 1

---
+++ Day24/pat1.py
@@ -0,0 +1,8 @@
+'''
+https://practice.geeksforgeeks.org/problems/square-pattern/1?page=1&status[]=solved&sortBy=submissions
+'''
+
+class Solution:
+    def printSquare(self, N):
+        for i in range(N):
+            print("* "*N)
